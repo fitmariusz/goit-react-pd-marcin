1ea4713d31d3e22ebe086d0dd8ef5e3fb9443135
fitmariusz <fitmariusz@gmail.com>
2024-04-17 10:40:41 +0200

    Update Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,25 +2,21 @@ import { useState, useEffect } from 'react'
 
 export const Counter = ({ initialCount = 0 }) => {
     const [ count, setCount ] = useState(initialCount)
-    const [ isStartClicked, setIsStartClicked ] = useState(false)
+    // const [ isStartClicked, setIsStartClicked ] = useState({start:0, stop:1})
     const [ intervalID, setIntervalID ] = useState(0)
 
     const onIncrement = () => {
         setCount(prev => prev + 1)
     }
-    
 
-    const onStartClicked = () => setIsStartClicked(true)
 
-    const onStopClicked = () => setIsStartClicked(false)
 
-    useEffect(() => {
-        if (isStartClicked)
-            setIntervalID(setTimeout(() => {
+    const onStartClicked = () => {setIntervalID(setTimeout(() => {
                 onIncrement()
-            }, 1000))
-        else clearInterval(intervalID)
-    }, [isStartClicked])
+            }, 1000))}
+
+    const onStopClicked = () => clearInterval(intervalID)
+
 
     useEffect(() => {
         if (count !== 0)
